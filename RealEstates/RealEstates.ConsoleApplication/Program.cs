944907a2d2bffdf8c07f376397675e3123eb13a6
evgeni136 <evgeni_136@abv.bg>
2022-05-18 23:36:30 +0300

    Create Db Models and initialize DbSets

---
+++ RealEstates/RealEstates.ConsoleApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealEstates.Data;
+
+class Program
+{
+    static void Main()
+    {
+        var db = new ApplicationDbContext();
+        db.Database.Migrate();
+    }
+}
---
+++ RealEstates/RealEstates.Models/Property.cs
@@ -0,0 +1,39 @@
+﻿namespace RealEstates.Models
+{
+
+    public class Property
+    {
+        public Property()
+        {
+            Tags = new HashSet<Tag>();
+        }
+
+        public int Id { get; set; }
+
+        public int Size { get; set; }
+
+        public int? YardSize { get; set; }
+
+        public int? Floor { get; set; }
+
+        public int? TotalFloors { get; set; }
+
+        public int? Year { get; set; }
+
+        public int? Price { get; set; }
+
+        public int DistrictId { get; set; }
+
+        public District District { get; set; }
+
+        public int TypeId { get; set; }
+
+        public PropertyType Type { get; set; }
+
+        public int BuildingTypeId { get; set; }
+
+        public BuildingType BuildingType { get; set; }
+
+        public virtual ICollection<Tag> Tags { get; set; }
+    }
+}
